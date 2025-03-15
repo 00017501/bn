aeb06e4efca364ff31aa32ee1fcb8906c6563f43
00017501 <00017501.wiut.student@gmail.com>
2025-03-15 09:43:10 +0500

    feat: initialize project
    - add static files (in public dir)
    - download and add bootstrap styles and scripts
    - configure eslint
    - add environmental vars for dev and prod
    - add readme.md file (not complete yet tho)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Boundless Narrative ✨
+
+![Project Banner](.github/readme-banner.png)
+
+## 🌌 Welcome to the Infinite Story Space
+
+In a digital realm where every voice deserves to be heard, Boundless Narrative emerges as a sanctuary for storytellers, dreamers, and truth-seekers. Here, stories transcend identity, and narratives know no bounds.
---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import js from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.commonjs
+      },
+      ecmaVersion: 2022,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    }
+  },
+  js.configs.recommended
+];
