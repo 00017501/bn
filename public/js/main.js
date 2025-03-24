c74603fa72a6050a45247f6cdad1ea0189e44ca5
00017501 <00017501.wiut.student@gmail.com>
2025-03-24 18:02:18 +0500

    feat: add library to support colorful codeblocks

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,10 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+// Initialize highlight.js for code blocks
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((el) => {
+    hljs.highlightElement(el);
+  });
+});
